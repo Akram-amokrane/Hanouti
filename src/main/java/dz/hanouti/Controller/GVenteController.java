1fda164b8e4ef776320f13b1c5517d3a5e593bd7
MIS <amokraneakram54@gmail.com>
2022-05-05 18:53:50 +0100

    commit

---
+++ src/main/java/dz/hanouti/Controller/GVenteController.java
@@ -0,0 +1,11 @@
+package dz.hanouti.Controller;
+
+import javafx.fxml.FXML;
+
+public class GVenteController extends WindowBarController {
+
+    @FXML
+    public void initialize(){
+        setWindowTitle("Gestion des ventes");
+    }
+}
--- src/main/java/dz/hanouti/View/GVente.java
+++ src/main/java/dz/hanouti/View/GVente.java
@@ -1,5 +1,6 @@
 package dz.hanouti.View;
 
+import dz.hanouti.Controller.GVenteController;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.paint.Color;
@@ -11,6 +12,7 @@ public class GVente {
     public GVente(Stage stage){
         try {
             FXMLLoader fxmlLoader = new FXMLLoader(Login.class.getResource("/fxml/GVente.fxml"));
+            //fxmlLoader.setController(new GVenteController());
             Scene scene = new Scene(fxmlLoader.load());
             scene.getStylesheets().add(getClass().getResource("/css/GVente.css").toExternalForm());
             scene.setFill(Color.TRANSPARENT);
